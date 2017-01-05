682554ad8881cb1e71bb50812593676603735d78
Kunchai Sodhom <trrekiiz@camphub.in.th>
2017-01-05 10:45:52 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : '',
+  password : '1234',
   database : 'kmutt_student'
 });
 
